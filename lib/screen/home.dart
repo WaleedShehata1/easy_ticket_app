fed9158ad0929e343caadcab797d595112519064
WaleedMob <116899879+WaleedMob@users.noreply.github.com>
2023-04-03 15:57:30 +0200

    waleed

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -20,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: 
         [
