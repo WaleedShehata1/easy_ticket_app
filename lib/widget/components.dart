56c5d53c5f7c7322478682745cbd7372a3555d80
AbdulhamedAshry <abdulhamedashry@gmail.com>
2023-05-18 19:03:23 +0300

    Abdulhamed

--- lib/widget/components.dart
+++ lib/widget/components.dart
@@ -22,6 +22,17 @@ class firstTime {
   static bool? getData({required String key}) {
     return sharedPreferences?.getBool(key);
   }
+
+  static Future<bool?> saveData({
+    required String key,
+    required dynamic value,
+  }) async {
+    if (value is String) return await sharedPreferences?.setString(key, value);
+    if (value is int) return await sharedPreferences?.setInt(key, value);
+    if (value is bool) return await sharedPreferences?.setBool(key, value);
+
+    return await sharedPreferences?.setDouble(key, value);
+  }
 }
 
 class darkMode {
@@ -38,16 +49,5 @@ class darkMode {
 
   static bool? getData({required String key}) {
     return sharedPreferences?.getBool(key);
-  }
-
-  static Future<bool?> saveData({
-    required String key,
-    required dynamic value,
-  }) async {
-    if (value is String) return await sharedPreferences?.setString(key, value);
-    if (value is int) return await sharedPreferences?.setInt(key, value);
-    if (value is bool) return await sharedPreferences?.setBool(key, value);
-
-    return await sharedPreferences?.setDouble(key, value);
   }
 }
