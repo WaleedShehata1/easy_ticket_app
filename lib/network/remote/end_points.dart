936ac1db4345ef23644795cb85db4f4d02577eb5
WaleedMob <116899879+WaleedMob@users.noreply.github.com>
2023-05-21 23:07:17 +0300

    waleed

--- lib/network/remote/end_points.dart
+++ lib/network/remote/end_points.dart
@@ -1,6 +1,8 @@
-const login = 'login';
+import '../../widget/components.dart';
 
-const show = 'show/';
+const login = 'login';
+final uid = CacheHelper.getData(key: 'uid');
+String show = 'show/$uid';
 const register = 'register';
 const logout = 'logout';
 const update = 'update';
