576e4c7745dfe481a33147260be88a9c3afe19bf
AbdulhamedAshry <abdulhamedashry@gmail.com>
2023-05-30 18:55:06 +0300

    Abdulhamed

--- lib/cubit/counter/counter_cubit.dart
+++ lib/cubit/counter/counter_cubit.dart
@@ -1,21 +1,25 @@
+import 'package:easy_ticket_app/network/remote/end_points.dart';
+import 'package:easy_ticket_app/widget/components.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'counter_states.dart';
+import 'package:easy_ticket_app/screen/home.dart';
 
 class CounterCubit extends Cubit<CounterStates> {
   CounterCubit() : super(CounterInitialState());
 
   static CounterCubit get(context) => BlocProvider.of(context);
-
+  double ticketPrice = CacheHelper.getData(key: 'metroTicket');
   int counter = 1;
-
-  void increment() {
+  increment() {
     counter++;
     emit(CounterIncrementState(counter));
+    ticketPrice = ticketPrice * counter;
   }
 
-  void decrement() {
+  decrement() {
     if (counter > 1) counter--;
     emit(CounterDecrementState(counter));
+    ticketPrice = ticketPrice * counter;
   }
 }
